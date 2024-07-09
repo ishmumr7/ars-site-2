2821971317d45ce0b79b4b4da725250c78fb8b84
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-07-09 23:11:23 +0600

    Navbar construction

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar container">
+      <span>Placeholder</span>
+      <ul>
+        <li>Home</li>
+        <li>Product</li>
+        <li>Custom Solution</li>
+        <li>About</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  );
+}
