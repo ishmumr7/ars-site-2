ff7cb9d9ceedd0607b060d705754c45ba811a281
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-07-14 16:52:30 +0600

    Added Contact Component and slight changes to HowItWorks and About module css files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Hero from "@/components/Home/Hero/Hero";
 import HowItWorks from "@/components/Home/HowItWorks/HowItWorks";
 import News from "@/components/Home/News/News";
+import Contact from "@/components/Home/Contact/Contact";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <HowItWorks />
       <News />
       <About />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Home/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import styles from "./Contact.module.css";
+
+export default function Contact() {
+  return (
+    <div className={styles.contact}>
+      <div className={styles.contact_main}>
+        <div className={styles.contact_container}></div>
+      </div>
+      <div className={styles.footer}></div>
+    </div>
+  );
+}
