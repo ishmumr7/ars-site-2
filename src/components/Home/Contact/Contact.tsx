a6e83b45faf112bfbb8e0aa2a74ac0ca72d264ef
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-07-15 00:36:21 +0600

    Added Contact Us Page

--- src/components/Home/Contact/Contact.tsx
+++ src/components/Home/Contact/Contact.tsx
@@ -1,12 +1,87 @@
 import styles from "./Contact.module.css";
+import telephone_img from "../../../../public/assets/telephone_converted.png";
+import Image from "next/image";
+import { CSSProperties } from "react";
+import {
+  FaSquareInstagram,
+  FaLinkedin,
+  FaFacebook,
+  FaLocationDot,
+} from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
 
 export default function Contact() {
+  const ImageStyles: CSSProperties = {
+    objectFit: "cover",
+    borderTopLeftRadius: "10px",
+    borderBottomLeftRadius: "10px",
+  };
+
   return (
     <div className={styles.contact}>
       <div className={styles.contact_main}>
         <div className={styles.contact_container}>
-          <div></div>
-          <div></div>
+          <div className={styles.contact_img}>
+            <div>
+              <Image
+                src={telephone_img}
+                alt="Contact Us"
+                fill
+                style={ImageStyles}
+              />
+            </div>
+          </div>
+          <div className={styles.contact_details}>
+            <div className={styles.item}>
+              <FaSquareInstagram />
+              <div className={styles.item_text}>
+                <div className={styles.item_title}>Instagram</div>
+                <div className={styles.item_other}>@arsystems.sa</div>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <FaLinkedin />
+              <div className={styles.item_text}>
+                <div className={styles.item_title}>LinkedIn</div>
+                <div className={styles.item_other}></div>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <FaFacebook />
+              <div className={styles.item_text}>
+                <div className={styles.item_title}>Facebook</div>
+                <div className={styles.item_other}></div>
+              </div>
+            </div>
+            <div className={`${styles.item} ${styles.item_big}`}>
+              <MdEmail />
+              <div className={`${styles.item_text} ${styles.item_text_big}`}>
+                <div className={styles.item_title}>Email&nbsp;:</div>
+                <div className={styles.item_other}>inquires@arsystems.org</div>
+              </div>
+            </div>
+            <div className={`${styles.item} ${styles.item_big}`}>
+              <MdEmail />
+              <div className={`${styles.item_text} ${styles.item_text_big}`}>
+                <div className={styles.item_title}>Phone&nbsp;No&nbsp;:</div>
+                <div className={styles.item_other}>
+                  +966&nbsp;50&nbsp;312&nbsp;4449
+                </div>
+              </div>
+            </div>
+            <div className={`${styles.item} ${styles.item_big}`}>
+              <FaLocationDot style={{ margin: "2px" }} />
+              <div className={`${styles.item_text} ${styles.item_text_big}`}>
+                <div style={{ fontSize: "16px" }} className={styles.item_title}>
+                  Address&nbsp;:
+                </div>
+                <div style={{ fontSize: "14px" }} className={styles.item_other}>
+                  R&D Office: 7012 Al Farazdaq St, Ad Dhubbat, Riyadh 12627,
+                  Saudi Arabia
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.footer}></div>
--- src/components/Home/News/News.tsx
+++ src/components/Home/News/News.tsx
@@ -1,8 +1,14 @@
 import styles from "./News.module.css";
-import NewsComponent from "./NewsComponent";
-import bg_image from "../../../../public/assets/warehouse_bg.png";
+import n_1 from "../../../../public/assets/N_1.jpeg";
+import { CSSProperties } from "react";
+import Image from "next/image";
 
 export default function News() {
+  const ImageStyles: CSSProperties = {
+    objectFit: "cover",
+    borderTopRightRadius: "10px",
+    borderTopLeftRadius: "10px",
+  };
   return (
     <div className={styles.news}>
       <div className={styles.news_header}>
@@ -10,7 +16,28 @@ export default function News() {
         <span className={styles.placeholder}></span>
       </div>
       <div className={styles.news_content}>
-        <NewsComponent image={bg_image} title="Test 1" text="Some Text" />
+        <div className={styles.card}>
+          <div className={styles.card_image}>
+            <Image src={n_1} alt="Image" fill style={ImageStyles} />
+          </div>
+          <div className={styles.card_content}>
+            <h2>Our Vision</h2>
+            <p>
+              Advanced Robotics Systems (ARS) is thrilled to announce its
+              official launch today (15th July 2024). The event marks a
+              significant milestone in advancing Industrial robotics and
+              automation technology in the Saudi Vision 20230, promising
+              revolutionary solutions for various industries.
+              <br />
+              "We thrive on solving the pain points of industry where automation
+              and robotics systems can be implemented. High-quality output,
+              required cycle time, and easy maintenance are some of our primary
+              objectives when building a system."
+              <br />
+              Founder & CTO - Abrar Hussain
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
